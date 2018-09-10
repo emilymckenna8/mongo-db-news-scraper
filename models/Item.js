4dc797660b410be1642fbfd7c23a09e8f5c95157
Emily McKenna <emilymckenna08@gmail.com>
2018-09-09 20:05:24 -0400

    retrying commit

---
+++ models/Item.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  albumName:{
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String, 
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: Boolean,
+    default: false
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  numComments: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
